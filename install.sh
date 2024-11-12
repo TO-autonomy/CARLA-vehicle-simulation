fc86d01dc39f2a1228336a6aa2ff0a831b3dd063
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-11-13 01:08:32 +0200

    Created install.sh script. This script installs CARLA simulator and all of the required python libraries to run vehicle simulation.

---
+++ install.sh
@@ -0,0 +1,32 @@
+CARLA_DOWNLOAD_URL="https://tiny.carla.org/carla-0-9-15-linux"
+CARLA_VERSION="0.9.15"
+
+set -e
+
+echo "Installing CARLA simulator (version: $CARLA_VERSION)..."
+echo "========================================"
+if [ -d "src/CARLASimulator" ]; then
+    echo "Directory src/CARLASimulator already exists."
+else
+    echo "Creating directory src/CARLASimulator..."
+    mkdir -p src/CARLASimulator
+    echo "Retriving CARLA simulator files..."
+    curl -L $CARLA_DOWNLOAD_URL -o src/CARLASimulator/CARLA.tar.gz
+    echo "Extracting CARLA simulator files..."
+    tar -xvzf src/CARLASimulator/CARLA.tar.gz -C src/CARLASimulator
+    rm src/CARLASimulator/CARLA.tar.gz
+fi 
+echo "========================================"
+echo "CARLA simulator installed successfully."
+
+echo "Installing CARLA Python library (version: $CARLA_VERSION)..."
+echo "========================================"
+pip install carla==$CARLA_VERSION
+echo "========================================"
+echo "CARLA Python library installed successfully."
+
+echo "Installing other necessary Python libraries..."
+echo "========================================"
+pip3 install -r requirements.txt
+echo "========================================"
+echo "Installation completed successfully."
