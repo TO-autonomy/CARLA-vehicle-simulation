6e786b139d718c2efba3d814e340d4522987c3ac
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-11-19 10:56:57 +0200

    Changed simulation code. Added command-line flag for hiding buildings in simulation (--toggle_off_buildings). Improved pedestrian spawning (although still causes issues).

--- src/generate_datasets.sh
+++ src/generate_datasets.sh
@@ -12,47 +12,79 @@ jupyter nbconvert \
 ## Generate simulation data for the different towns
 
 # Town2
-python3 run_simulation.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town02.path.json \
- --output_dir /media/leppsalu/SSD_Storage/generated_data_town02 \
- --skip_validation
+# python3 run_simulation.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town02.path.json \
+#  --output_dir /media/leppsalu/SSD_Storage/generated_data_town02 \
+#  --skip_validation
 
-python3 run_simulation_postprocessing.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --input_dir /media/leppsalu/SSD_Storage/generated_data_town02 \
- --output_dir /media/leppsalu/SSD_Storage/processed_data_town02 \
- --n_frames_per_bag 1800
+# python3 run_simulation_postprocessing.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --input_dir /media/leppsalu/SSD_Storage/generated_data_town02 \
+#  --output_dir /media/leppsalu/SSD_Storage/processed_data_town02 \
+#  --n_frames_per_bag 1800
 
-# Town3
-python3 run_simulation.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town03.path.json \
- --output_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
- --skip_validation
+# Town3 1st route
+# python3 run_simulation.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town03.path.json \
+#  --output_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
+#  --skip_validation
 
-python3 run_simulation_postprocessing.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --input_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
- --output_dir /media/leppsalu/SSD_Storage/processed_data_town03 \
- --n_frames_per_bag 1800
+# python3 run_simulation_postprocessing.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --input_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
+#  --output_dir /media/leppsalu/SSD_Storage/processed_data_town03 \
+#  --n_frames_per_bag 1800
 
-# Town10
-python3 run_simulation.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town10.path.json \
- --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town10 \
- --skip_validation
+#  # Town3 2nd route
+# python3 run_simulation.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town03-1.path.json \
+#  --output_dir /media/leppsalu/SSD_Storage/generated_data_town03-1 \
+#  --skip_validation
+
+# python3 run_simulation_postprocessing.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --input_dir /media/leppsalu/SSD_Storage/generated_data_town03-1 \
+#  --output_dir /media/leppsalu/SSD_Storage/processed_data_town03-1 \
+#  --n_frames_per_bag 1800
+
+# # Town10
+# python3 run_simulation.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town10.path.json \
+#  --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town10 \
+#  --skip_validation
  
 
+# python3 run_simulation_postprocessing.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --input_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town10 \
+#  --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/processed_data_town10 \
+#  --n_frames_per_bag 1800
+
+# Town3 sample route
+python3 run_simulation.py \
+ --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+ --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+ --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town03-2.path.json \
+ --output_dir /media/leppsalu/SSD_Storage/generated_data_town03-2 \
+ --skip_validation \
+ --toggle_off_buildings
+
 python3 run_simulation_postprocessing.py \
  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --input_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town10 \
- --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/processed_data_town10 \
+ --input_dir /media/leppsalu/SSD_Storage/generated_data_town03-2 \
+ --output_dir /media/leppsalu/SSD_Storage/processed_data_town03-2 \
  --n_frames_per_bag 1800
+
