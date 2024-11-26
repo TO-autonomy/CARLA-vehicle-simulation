37534c6c70f45dc0746863795da74d1e6a26e0bd
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-11-26 20:08:43 +0200

    Changed simulation and post processing code. Replaced semantic cameras with instance segmentation cameras in simulation and accomodated these changes in post processing.

--- src/generate_datasets.sh
+++ src/generate_datasets.sh
@@ -73,18 +73,17 @@ jupyter nbconvert \
 #  --n_frames_per_bag 1800
 
 # Town3 sample route
-python3 run_simulation.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town03-2.path.json \
- --output_dir /media/leppsalu/SSD_Storage/generated_data_town03-2 \
- --skip_validation \
- --toggle_off_buildings
+# python3 run_simulation.py \
+#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+#  --episode_config /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/town02.path.json \
+#  --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town02-discardable \
+#  --skip_validation 
 
 python3 run_simulation_postprocessing.py \
  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
- --input_dir /media/leppsalu/SSD_Storage/generated_data_town03-2 \
- --output_dir /media/leppsalu/SSD_Storage/processed_data_town03-2 \
+ --input_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/generated_data_town02-discardable_sample \
+ --output_dir /home/leppsalu/Desktop/Github/CARLA-vehicle-simulation/src/processed_data_town02-discardable_sample \
  --n_frames_per_bag 1800
 
