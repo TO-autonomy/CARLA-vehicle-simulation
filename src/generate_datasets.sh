c64b7f22352cb695eccc80e5808ad0d3e33eb058
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-12-11 10:59:56 +0200

    Updated generate_datasets.sh. Added code that checks whether CARLA simulator is running before starting the simulation script.

--- src/generate_datasets.sh
+++ src/generate_datasets.sh
@@ -9,6 +9,16 @@ jupyter nbconvert \
 jupyter nbconvert \
     --to script run_simulation_postprocessing.ipynb
 
+# Check if the CARLA simulator is running
+echo "Checking if the CARLA simulator is running..."
+if pgrep -f "CarlaUE4" > /dev/null
+then
+    echo "CARLA simulator is running. Proceeding with the dataset generation."
+else
+    echo "CARLA simulator is not running. Please start the simulator and try again."
+    exit 1
+fi
+
 ## Generate simulation data for the different towns
 
 # Town2 sample route
@@ -27,20 +37,16 @@ jupyter nbconvert \
 #  --n_frames_per_bag 1800 &
 
 # Town3 sample route
-# python3 run_simulation.py \
-#  --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/voxel-visibility/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
-#  --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/voxel-visibility/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
-#  --episode_config /home/leppsalu/Desktop/Github/voxel-visibility/CARLA-vehicle-simulation/src/config//town03.path.json \
-#  --output_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
-#  --skip_validation 
+python3 run_simulation.py \
+ --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/voxel-visibility-multithreaded/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+ --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/voxel-visibility-multithreaded/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+ --episode_config /home/leppsalu/Desktop/Github/voxel-visibility-multithreaded/CARLA-vehicle-simulation/src/config/town03.path.json \
+ --output_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
+ --skip_validation 
 
 python3 run_simulation_postprocessing.py \
- --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/voxel-visibility/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
- --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/voxel-visibility/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
+ --ego_vehicle_extrinsics /home/leppsalu/Desktop/Github/voxel-visibility-multithreaded/CARLA-vehicle-simulation/src/config/carla_extrinsics.urdf \
+ --ego_vehicle_intrinsics /home/leppsalu/Desktop/Github/voxel-visibility-multithreaded/CARLA-vehicle-simulation/src/config/carla_intrinsics.json \
  --input_dir /media/leppsalu/SSD_Storage/generated_data_town03 \
- --output_dir /media/leppsalu/HDD_Storage/processed_data_town03 \
+ --output_dir /media/leppsalu/SSD_Storage/processed_data_town03 \
  --n_frames_per_bag 1800
-
-
-
-
