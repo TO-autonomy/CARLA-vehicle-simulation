286f4ce3bb451de34cdec4c8a2884109c0a9edc3
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-12-11 10:26:18 +0200

    Updated install.sh. Added pip3 version check. This is important because necessary carla python library is only available with newer pip3 versions.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 CARLA_DOWNLOAD_URL="https://tiny.carla.org/carla-0-9-15-linux"
 CARLA_VERSION="0.9.15"
+REQUIRED_PIP_VERSION="24.3.1"
 
 set -e
 
 echo "Installing CARLA simulator (version: $CARLA_VERSION)..."
-echo "========================================"
 if [ -d "src/CARLASimulator" ]; then
     echo "Directory src/CARLASimulator already exists."
 else
@@ -16,17 +16,35 @@ else
     tar -xvzf src/CARLASimulator/CARLA.tar.gz -C src/CARLASimulator
     rm src/CARLASimulator/CARLA.tar.gz
 fi 
-echo "========================================"
 echo "CARLA simulator installed successfully."
 
+echo "========================================"
+
+CURRENT_PIP_VERSION=$(pip3 --version | awk '{print $2}')
+version_greater_equal() {
+    printf '%s\n%s\n' "$1" "$2" | sort -V -C
+}
+
+echo "Checking pip3 version..."
+if version_greater_equal "$REQUIRED_PIP_VERSION" "$CURRENT_PIP_VERSION"; then
+    echo "pip3 current version $CURRENT_PIP_VERSION, satisfies minimum required version $REQUIRED_PIP_VERSION."
+    echo "pip3 version check completed successfully."
+else
+    echo "pip3 version $CURRENT_PIP_VERSION is older than minimum required version $REQUIRED_PIP_VERSION. Upgrading pip3..."
+    pip3 install --upgrade pip
+    echo "pip3 upgraded successfully."
+fi
+
+echo "========================================"
+
 echo "Installing CARLA Python library (version: $CARLA_VERSION)..."
-echo "========================================"
 pip3 install carla==$CARLA_VERSION
-echo "========================================"
 echo "CARLA Python library installed successfully."
 
+echo "========================================"
+
 echo "Installing other necessary Python libraries..."
-echo "========================================"
 pip3 install -r requirements.txt
-echo "========================================"
 echo "Installation completed successfully."
+
+echo "========================================"
