813e6c7449fdc5a3f5924c075bcd374bb423bdc8
Jyrgen Leppsalu <leppsalujyrgen@gmail.com>
2025-10-28 16:05:50 +0200

    Changed start_simulation.sh. New functionality allows to restart the simulator if one instance is already running.

--- start_simulator.sh
+++ start_simulator.sh
@@ -5,19 +5,34 @@ CURRENT_DIR=$(pwd)
 SOURCE_DIR=$CURRENT_DIR/src
 SIMULATOR_DIR=$SOURCE_DIR/CARLASimulator
 SERVER_SCRIPT="$SIMULATOR_DIR/CarlaUE4.sh"
-SERVER_SCRIPT_BASENAME=$(basename "$SERVER_SCRIPT")
+SERVER_SCRIPT_BASENAME="CarlaUE4"
+SERVER_BOOT_TIME=10 # Time to wait for the server to boot up (in seconds); Can be adjusted based on system performance.
+
 
 echo "Checking if the CARLA simulator is running..."
-if ! pgrep -af "$SERVER_SCRIPT_BASENAME" | grep -v "$0" > /dev/null; then
+SERVER_PID=$(pgrep -af "$SERVER_SCRIPT_BASENAME" | grep -v "$0" | awk '{print $1}')
+if [ -z "$SERVER_PID" ]; then
     echo "CARLA simulator is not running."
     echo "Starting server..."
     # Start server script and redirect only stdout to /dev/null
     sh "$SERVER_SCRIPT" > /dev/null &
-    sleep 5
-    if ! pgrep -af "$SERVER_SCRIPT_BASENAME" | grep -v "$0" > /dev/null; then
-        echo "Failed to start CARLA simulator. Exiting..."
-        exit 1
+    sleep $SERVER_BOOT_TIME
+else
+    echo "CARLA simulator is already running. (PID: $SERVER_PID)"
+    read -p "Do you want to restart the CARLA simulator? (y/n): " RESTART_CARLA
+    if [ "$RESTART_CARLA" = "y" ]; then
+        echo "Restarting CARLA simulator..."
+        kill -9 "$SERVER_PID"
+        sleep 2
+        # Start server script and redirect only stdout to /dev/null
+        sh "$SERVER_SCRIPT" > /dev/null &
+        sleep $SERVER_BOOT_TIME
     fi
+fi
+
+if ! pgrep -af "$SERVER_SCRIPT_BASENAME" | grep -v "$0" > /dev/null; then
+    echo "Failed to start CARLA simulator. Exiting..."
+    exit 1
 fi
 
 echo "CARLA simulator is running."
